c25a2340ccd1ab8f8b9b20300674be532c78ca08
floatit <info@addfloat.com>
2022-07-22 08:55:20 +0530

    config image

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     minimumCacheTTL: 31536000,
-    domains: ["data.vacancyjobalert.com", "cdnaws.vacancyjobalert.com", "lh3.googleusercontent.com", "sin1.contabostorage.com", "1df7b34158320f40e67a0fe217e4ce2c.r2.cloudflarestorage.com"],
+    domains: ["o8o8.or11.idrivee2-12.com","data.vacancyjobalert.com", "cdnaws.vacancyjobalert.com", "lh3.googleusercontent.com", "sin1.contabostorage.com", "1df7b34158320f40e67a0fe217e4ce2c.r2.cloudflarestorage.com"],
   },
 }
 
