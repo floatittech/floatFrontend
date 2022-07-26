7dba5e018916b8bd1c926f38b2d7dfe39704184a
floatit <info@addfloat.com>
2022-07-26 22:21:02 +0530

    login update

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import GoogleButton from 'react-google-button'
 import { signIn, useSession } from "next-auth/react"
 import { useRouter } from "next/router";
@@ -6,20 +5,15 @@ import LoadingScreen from '../components/LoadingScreen';
 
 export default function Login() {
 
-  const { status } = useSession();
+  const {data:session , status } = useSession();
   const router = useRouter();
 
-  if(status === 'authenticated'){
-   router.push("/workfloat");
-  } else {
-   <LoadingScreen/>
-  }
-
+  if(status === 'authenticated') router.push("/workfloat");
+  if(status === 'loading' || session) return <LoadingScreen/>
 
   return (
-      <div className="flex justify-center h-screen items-center">
-        <GoogleButton onClick={() => signIn("google", { callbackUrl: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/workfloat/` }, { prompt: "login" })}/>
-      </div>
-      )
-  
+    <div className="flex justify-center h-screen items-center">
+    <GoogleButton onClick={() => signIn("google", { callbackUrl: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/workfloat/` }, { prompt: "login" })}/>
+    </div>
+  )
 }
