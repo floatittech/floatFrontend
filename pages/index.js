f6e4eb7428ddbecc39460a966051692b64f17ade
floatit <info@addfloat.com>
2022-07-26 19:21:01 +0530

    index update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
 import LoadingScreen from "../components/LoadingScreen";
 
 export default function Home() {
+  const router = useRouter();
+  router.push("/workfloat");
+
   return <LoadingScreen/>
 }
 
